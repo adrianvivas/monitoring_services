2b61c615665db514ddfcf26e61bd4cb3627cce32
Adrian Vivas <adrianj.vivasb@gmail.com>
2020-05-14 18:28:09 -0500

    better info render

--- app.py
+++ app.py
@@ -5,12 +5,21 @@ import psutil
 app = Flask(__name__)
 api = Api(app)
 
+class Main(Resource):
+    def get(self):
+        return jsonify({"message": "This site shows you the main system info"})
+
 class GetStorage(Resource):
     def get(self):
         storage = psutil.disk_usage('/')
         return jsonify({
             "message": "System storage",
-            "value": storage
+            "value": {
+                "Total": storage[0],
+                "Used": storage[1],
+                "Free": storage[2],
+                "Percent": storage[3]
+                }
             })
     
 class GetMemory(Resource):
@@ -18,11 +27,18 @@ class GetMemory(Resource):
         memory = psutil.virtual_memory()
         return jsonify({
             "message": "RAM memory",
-            "value": memory
+            "value": {
+                "Total": memory[0],
+                "Avaliable": memory[1],
+                "Percent": memory[2],
+                "Used": memory[3],
+                "Free": memory[4]
+                }
             })
 
 api.add_resource(GetStorage, '/storage')
 api.add_resource(GetMemory, '/memory')
+api.add_resource(Main, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
